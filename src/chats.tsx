ca43ae0640a2c34a1c6b95d44872898f6892581d
Xavi Fortes <itsfortes@gmail.com>
2022-10-20 16:46:53 +0200

    Redirect to login if token expired

--- src/chats.tsx
+++ src/chats.tsx
@@ -6,6 +6,20 @@ import Chats from './apps/Chats';
 import Messages from './apps/Messages';
 import { createSignal } from 'solid-js';
 
+// Redirect to login if not cookie logged in
+if (document.cookie === "") {
+    window.location.href = "/login";
+}
+
+
+// Check if cookie jwt is expired and redirect to login
+const jwt = document.cookie.split("=")[1];
+const jwtPayload = JSON.parse(atob(jwt.split(".")[1]));
+const jwtExp = jwtPayload.exp;
+const now = Math.floor(Date.now() / 1000);
+if (now > jwtExp) {
+    window.location.href = "/login.html";
+}
 
 
 render(() => <Chats />, document.getElementById('chats') as HTMLElement);
